10c3d455d049956f7acc53244e106353840e434c
naihai <448435279@qq.com>
2016-05-05 22:09:40 -0500

    update the copyright file. modify the date()

--- html/copyright.php
+++ html/copyright.php
@@ -3,7 +3,7 @@
     <div class="hustca-row">
         <div class="col-21-24">
             <div class="hustca-footer-info" >
-                ©2015--2016 HUST CA , HuaZhong University of Science and Technology | All rights reserved.<a href="http://www.miitbeian.gov.cn/" target="_blank">皖ICP备15020446号-1</a>
+                ©2015--<?php echo date("Y")?> HUST CA , HuaZhong University of Science and Technology | All rights reserved.<a href="http://www.miitbeian.gov.cn/" target="_blank">皖ICP备15020446号-1</a>
             </div>
         </div>
     </div>
