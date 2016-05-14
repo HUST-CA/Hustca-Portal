b7e8882db501db494082ea4ceb136ee5f02e5dd0
naihaishy <448435279@qq.com>
2016-05-14 22:21:54 +0800

    delete the table create

--- html/jr/php/webapply/conn.php
+++ html/jr/php/webapply/conn.php
@@ -2,4 +2,4 @@
 
 require_once("../../../config/db-conn.php");
 
-mysql_query("create table apply(id int unsigned not null auto_increment primary key,name char(10) character set utf8 not null,sex char(6) not null,tel char(11) not null,email char(30) not null unique key,website char(20) character set utf8 not null,it_group char(3) not null,experience varchar(300) character set utf8,excellent varchar(150) character set utf8);");
+//mysql_query("create table apply(id int unsigned not null auto_increment primary key,name char(10) character set utf8 not null,sex char(6) not null,tel char(11) not null,email char(30) not null unique key,website char(20) character set utf8 not null,it_group char(3) not null,experience varchar(300) character set utf8,excellent varchar(150) character set utf8);");
--- html/lx/php/lx/conn.php
+++ html/lx/php/lx/conn.php
@@ -1,3 +1,3 @@
 <?php
 require_once("../../../config/db-conn.php");
-mysql_query("create table lxwm(id int unsigned not null auto_increment primary key,name char(10) character set utf8 not null,email varchar(30) not null,subject varchar(200) not null,tel char(11) not null,content varchar(2000) character set utf8 not null);");
+//mysql_query("create table lxwm(id int unsigned not null auto_increment primary key,name char(10) character set utf8 not null,email varchar(30) not null,subject varchar(200) not null,tel char(11) not null,content varchar(2000) character set utf8 not null);");
